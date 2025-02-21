a83f25887a86a78114285799023842e31e109049
ivanexDev <ivanexdev@gmail.com>
2025-02-21 14:34:58 -0300

    dia 2 - crypto

--- modulos-node/04-crypto-b.js
+++ modulos-node/04-crypto-b.js
@@ -3,10 +3,6 @@ const crypto = require("crypto");
 const algorithm = "aes-256-cbc";
 const key = crypto.randomBytes(32); //Clave de 32 bytes
 
-
-
-
-
 const iv = crypto.randomBytes(16); //Vector de inicializaón (IV)
 
 const cipher = crypto.createCipheriv(algorithm, key, iv);
